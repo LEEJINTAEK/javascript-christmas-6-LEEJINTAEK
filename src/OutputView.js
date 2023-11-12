eabb4ae3d10adb68623cdfba64229a37c2339a17
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-12 18:47:09 +0900

    feat: 출력 구현 함수

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,8 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  printMenu() {
+  printMenu(menuMap) {
     MissionUtils.Console.print('<주문 메뉴>');
+    for (const [menu, count] of menuMap) {
+      MissionUtils.Console.print(`${menu} ${count}개`);
+    }
   },
   printIntroduce() {
     MissionUtils.Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
@@ -18,19 +21,24 @@ const OutputView = {
     MissionUtils.Console.print('<증정 메뉴>');
     MissionUtils.Console.print(giftMenu);
   },
-  printBenefit() {
+  printBenefit(dictBenefit) {
     MissionUtils.Console.print('<혜택 내역>');
+    for (const [event, sale] of Object.entries(dictBenefit)) {
+      if (sale > 0) {
+        MissionUtils.Console.print(`${event}: ${(sale * -1).toLocaleString()}원`);
+      }
+    }
   },
   printTotalBenefit(benefitAmount) {
     MissionUtils.Console.print('<총혜택 금액>');
-    MissionUtils.Console.print(benefitAmount * (-1).toLocaleString());
+    MissionUtils.Console.print((benefitAmount * -1).toLocaleString());
   },
   printBill(pay) {
     MissionUtils.Console.print('<할인 후 예상 결제 금액>');
     MissionUtils.Console.print(pay.toLocaleString());
   },
   printBadge(badge) {
-    MissionUtils.Console.print('<할인 후 예상 결제 금액>');
+    MissionUtils.Console.print('<12월 이벤트 배지>');
     MissionUtils.Console.print(badge);
   },
 };
