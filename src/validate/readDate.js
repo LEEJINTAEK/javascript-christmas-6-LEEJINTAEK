3678b368e9bf9540166930bc3c555203bc555315
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-12 20:38:20 +0900

    feat: 요구사항에 따른 예외처리 구현

---
+++ src/validate/readDate.js
@@ -0,0 +1,13 @@
+class readDate {
+  constructor(date) {
+    this.#validate(date);
+  }
+  // 1~31 숫자가 아닐 때 처리
+  #validate(date) {
+    if (isNaN(date) || (date >= 1 && date <= 31)) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
+  }
+}
+
+export default readDate;
---
+++ src/validate/readMenu.js
@@ -0,0 +1,49 @@
+import MENUS from '../domain/menu.js';
+
+const MENU_ERROR_MESSAGE = '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.';
+
+class readMenu {
+  #menu = new Map();
+
+  //menus->2차원배열
+  constructor(menus) {
+    this.#validate(menus);
+    this.#duplication(menus);
+  }
+
+  #validate(menus) {
+    for (const menu of menus) {
+      //입력 형식에 맞지 않을 때, 메뉴 개수가 1미만일 때
+      if (menu.length !== 2 || Number(menu[1]) < 1) {
+        throw new Error(MENU_ERROR_MESSAGE);
+      }
+      //메뉴가 없을 때
+      this.#notOnTheMenu(menu[0]);
+    }
+  }
+
+  //메뉴가 중복일 때
+  #duplication(menus) {
+    for (const [menu, count] of menus) {
+      if (this.#menu.has(menu)) {
+        throw new Error(MENU_ERROR_MESSAGE);
+      }
+      this.#menu.set(menu, count);
+    }
+  }
+
+  //메뉴가 없을 때
+  #notOnTheMenu(menu) {
+    let found = false;
+    for (const category in MENU) {
+      if (MENUS[category][menu]) {
+        found = true;
+      }
+    }
+    if (!found) {
+      throw new Error(MENU_ERROR_MESSAGE);
+    }
+  }
+}
+
+export default readMenu;
