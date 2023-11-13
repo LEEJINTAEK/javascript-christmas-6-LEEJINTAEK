2a6b9d31b3c5c0faec90b48077bc7aafdc81df7f
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-13 21:19:56 +0900

    test: 혜택 내역 표시 테스트 케이스 추가

--- __tests__/EventDiscountTest.js
+++ __tests__/EventDiscountTest.js
@@ -15,7 +15,7 @@ describe('EventDiscount', () => {
 
   test('specialDiscount 정확도', () => {
     const expectedDiscount = 1000;
-    expect(eventDiscount.specailDiscount()).toEqual(expectedDiscount);
+    expect(eventDiscount.specialDiscount()).toEqual(expectedDiscount);
   });
 
   test('weekdayDiscount 정확도', () => {
@@ -30,5 +30,27 @@ describe('EventDiscount', () => {
     const menu = '티본스테이크';
     const count = 1;
     expect(eventDiscount.weekDiscount(menu, count)).toEqual(expectedDiscount);
+  });
+
+  test('getDiscountContents 정확도', () => {
+    const menu = '초코케이크';
+    const count = 2;
+
+    // 먼저 discount를 계산하도록
+    eventDiscount.dDayDiscount();
+    eventDiscount.specialDiscount();
+    eventDiscount.weekDiscount(menu, count);
+
+    const discountContents = eventDiscount.getDiscountContents();
+
+    // 예상 결과 설정
+    const expectedContents = {
+      '크리스마스 디데이 할인': 1900,
+      '특별 할인': 1000,
+      '평일 할인': 4046,
+    };
+
+    // 결과 비교
+    expect(discountContents).toEqual(expectedContents);
   });
 });
